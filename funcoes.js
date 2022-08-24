57fbf2b09f0d78d48c8a65a3ff020f5bb925e529
canteirogabi <gabriella.canteiro@estudante.ifms.edu.br>
2022-08-24 10:12:44 -0400

    criamos funções js

---
+++ funcoes.js
@@ -0,0 +1,33 @@
+var numEsco=[]
+var numSort = []
+
+function sortNumeros (){
+
+for (let i = 0; i < 6; i++){
+    
+let num = Math.ceil(Math.random() * 60)
+console.log(num)
+if(!numSort.includes(num)){
+    numSort[i] = num;
+} else {
+    i = i-1;
+}}
+
+console.log(numSort)
+
+}
+
+function lerNumero(value, pos){
+    if (value < 1){
+        alert("O  número não pode ser zero ou negativo! Digite novamente.")
+    } else if (value > 60){
+        alert ("O  número não pode ser maior que 60! Digite novamente.")
+    } else if (numEsco.includes(value)){
+        alert("Número repetido! Digite novamente!")
+    } else {
+        numEsco [pos] = value
+        console.log(numEsco)
+    }
+
+}
+
