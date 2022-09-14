9408f299df44e90a787351947dc4138af3812909
canteirogabi <gabriella.canteiro@estudante.ifms.edu.br>
2022-09-14 09:48:36 -0400

    finalizando

--- funcoes.js
+++ funcoes.js
@@ -1,33 +1,42 @@
-var numEsco=[]
+var numEsco = []
 var numSort = []
 
-function sortNumeros (){
+function sortNumeros(){
 
-for (let i = 0; i < 6; i++){
-    
-let num = Math.ceil(Math.random() * 60)
-console.log(num)
-if(!numSort.includes(num)){
-    numSort[i] = num;
-} else {
-    i = i-1;
-}}
-
-console.log(numSort)
-
+    for(let i = 0; i< 6; i++){
+    let num = Math.ceil (Math.random() * 60)
+    console.log(num)
+    if(!numSort.includes(num) ){
+        numSort[i] = num
+    }else{
+    i = i -1;
+    }
+}
+document.getElementById("sorteados").innerHTML = numSort;
+contarAcertos();
 }
 
 function lerNumero(value, pos){
-    if (value < 1){
-        alert("O  número não pode ser zero ou negativo! Digite novamente.")
-    } else if (value > 60){
-        alert ("O  número não pode ser maior que 60! Digite novamente.")
-    } else if (numEsco.includes(value)){
-        alert("Número repetido! Digite novamente!")
-    } else {
-        numEsco [pos] = value
-        console.log(numEsco)
+    let num = Number (value)
+    if(num < 1){
+        alert("Numero não pode ser zero ou negativo! Digite novamente!")
+    }else if(num > 60){
+        alert("Numero não pode ser maior que 60! Digite novamente!")
+    }else if (numEsco.includes(num)){
+        alert ("Numero repetido! Digite novamente!")
+    }else{
+    numEsco[pos] = num
+    console.log(numEsco)
     }
-
+    
 }
 
+function contarAcertos(){
+    let cont=0;
+    numEsco.forEach(function(value,index){ 
+        if(numSort.includes(value)){
+            cont=cont+1;
+        }
+    })
+    document.getElementById("acertos").innerHTML = cont;
+}
